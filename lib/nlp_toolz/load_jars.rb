3d7008f63f3e9dd7c6ed6b252261b297a3d756a1
LeFnord <pscholz.le@gmail.com>
2013-02-27 15:48:37 +0100

    added celluloid for concurency

--- lib/nlp_toolz/load_jars.rb
+++
@@ -1,8 +0,0 @@
-module NlpToolz
-  CLASS_PATH = [
-                 File.join(File.dirname(__FILE__), '..', '..', "jars/jwnl-1.3.3.jar"),
-                 File.join(File.dirname(__FILE__), '..', '..', "jars/opennlp-tools-1.5.2.jar"),
-                 File.join(File.dirname(__FILE__), '..', '..', "jars/opennlp-maxent-3.0.2.jar")
-               ].join(":")
-  Rjb::load(CLASS_PATH,['-Xmx1024m','-Djava.awt.headless=true'])
-end
